2350ecaeb3210e108ef6e087d800031a696864f9
hashimmalik59 <hm3630450@gmail.com>
2024-06-26 22:33:54 -0700

    123

---
+++ script.js
@@ -0,0 +1,11 @@
+let count = 0;
+function sum() {
+  count += 1;
+  let sum = document.querySelector("input").value;
+  sum = count += 1;
+}
+function reset() {
+  count = 0;
+  let reset = document.querySelector("input").value;
+  reset = count *= 0;
+}
